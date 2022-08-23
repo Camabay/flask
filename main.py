a6e0ef004118a0270fc8e7b40e343cd5cd491b76
alexi <84298152+Camabay@users.noreply.github.com>
2022-08-23 15:33:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import requests
 
-@app.route("/")
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
 def server_info():
-    return "Hola Mundo Flask"
+    return render_template(index.html)
 
 if __name__ == "__main__":
     app.run()
